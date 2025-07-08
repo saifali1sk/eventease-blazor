6d9a6babdb401cf33ba5af8c579a1046fc8d7fd3
saifali1sk <saifaliscl1@gmail.com>
2025-07-09 00:25:21 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using EventEase;
+using EventEase.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Register custom services
+builder.Services.AddScoped<EventService>();
+builder.Services.AddScoped<UserService>();
+
+await builder.Build().RunAsync();
